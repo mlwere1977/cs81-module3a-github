cc41a75a851e67e441b1780e84390244cd753839
Mukwaya Lwere <lwere.lwere@gmail.com>
2025-07-03 15:09:14 +0000

    Add Hellow World simulation script with reflection and analysis

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+// Name: Mukwaya Lwere
+// Class: CS 81 JavaScript
+//Module 2 Assignment 2A: Hello, World! with External JavaScript
+// Date: 06/29/2025
+
+// Create a variable to store your message
+let message = "Hello, World!";
+
+// Display the message using an alert popup
+alert(message);
+
+// Output the message to the browser's developer console
+console.log(message);
